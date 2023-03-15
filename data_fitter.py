a8d0f067f439ee7e146d42e7e68f5ca3eef04024
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2023-03-15 09:55:46 +0000

    Added initial data fitter

---
+++ data_fitter.py
@@ -0,0 +1,19 @@
+# Library import
+import numpy as np
+import scipy as sp
+
+# Curve fitters
+
+
+# Gyrosynchrontron emission fitter
+# def gyro_fitter():
+#     def model():
+#         return some_model
+#     return 
+
+
+# Plasma emission fitter
+# def plas_fitter():
+#     def model():
+#         return some_model
+#     return
