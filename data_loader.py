681b75b751959d2641fe3f99250a6cd2be243955
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2023-03-26 15:32:22 +0100

    Updated data loader

--- data_loader.py
+++ data_loader.py
@@ -4,10 +4,14 @@ import datetime
 
 # CSV data parser
 def csv_loader(file_path, dtype=float):
+    # Initial data load with specified dtype
+    data = np.loadtxt(file_path, delimiter=",", dtype=dtype)
+
+    # Data transposer if there are more columns than rows
+    data = data.transpose() if data.shape[1] > data.shape[0] else data
+
     # Time data loader and convertor
     if dtype == np.uint64:
-        # Load time data
-        data = np.loadtxt(file_path, delimiter=",", dtype=np.uint64)
         # Define start point of datetime as day01
         time_base = datetime.datetime(1979, 1, 1) - datetime.timedelta(days=1)
         # Construct time function for calculating time result
@@ -15,19 +19,11 @@ def csv_loader(file_path, dtype=float):
             time_base + datetime.timedelta(milliseconds=ms + days * 86400000)
             for ms, days in data
         ]
-        # Convert days since time base to YYYY-MM-DD
-        # time_days = [dt.strftime("%Y-%m-%d") for dt in time_repo]
-        # Convert milliseconds since the day to HH:MM:SS
-        # time_prec = [dt.strftime("%H:%M:%S") for dt in time_repo]
         # Combined datetime data
-        time_data = [dt.strftime("%Y-%m-%d %H:%M:%S") for dt in time_repo]
-        # Combined datetime data
-        time_data = np.array(time_data)
+        time_data = np.array([dt.strftime("%Y-%m-%d %H:%M:%S") for dt in time_repo])
         # Return converted time data array
         return time_data
     # Data loader for all other dtype
     else:
-        # Load all other type of data
-        data = np.loadtxt(file_path, delimiter=",", dtype=dtype)
         # Return data
         return data
