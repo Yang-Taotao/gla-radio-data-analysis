fc6a85a6380dce8e178d35c7384f97e48e9fa2e7
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2023-03-30 21:05:12 +0100

    Fitter refactor, general cases

--- data_fitter.py
+++ data_fitter.py
@@ -7,21 +7,34 @@ Created on Wed Mar 15 2023
 """
 # Library import
 import numpy as np
-import scipy as sp
+from scipy.optimize import curve_fit
 
-# Preliminary data combiner around peak
+# Radio data curve fit model functions repo
+def model_func(arg):
+    # Local variable repo
+    data_freq, A, B, a, b, c, k = [arg[i] for i in range(len(arg))]
+    # Function assignment
+    flux_gyro, flux_plas = (
+        A * data_freq**a * (1 - np.exp(-B * data_freq**(-b))),
+        c * data_freq**k,
+    )
+    # Function repo
+    flux_model = (flux_gyro, flux_plas)
+    # Function return
+    return flux_model
 
 
-# Curve fitters
-# Gyrosynchrontron emission fitter
-# def gyro_fitter():
-#     def model():
-#         return some_model
-#     return
-
-
-# Plasma emission fitter
-# def plas_fitter():
-#     def model():
-#         return some_model
-#     return
+# General curve fitter
+def curve_fitter(model_string, data_x, data_y, arg):
+    # Model selector
+    if model_string = "gyro":
+        model, p0 = (
+            model_func(arg)[0],  # Get gyro model
+            [1, 1, 1, 1],  # A, B, a, b initial guess
+        )
+    elif model_string = "plas":
+        model, p0 = (
+            model_func(arg)[1],  # Get plas model
+            [1, 1],  # c, k initial guess
+        )
+    # Fitter
